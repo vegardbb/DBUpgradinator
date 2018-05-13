61f4e6a77e256feb23e9c049aab0c7948cd4479c
Vegard Bjerkli Bugge <vegardbb@stud.ntnu.no>
2018-05-13 20:48:41 +0200

    Change return declaration

--- src/vbb/dbupgradinator/StringQueryInterface.java
+++ src/vbb/dbupgradinator/StringQueryInterface.java
@@ -2,5 +2,5 @@ package vbb.dbupgradinator;
 
 public interface StringQueryInterface {
     public String query(String key);
-    public String persist(String key, String aggregate);
+    public void persist(String key, String aggregate);
 }
